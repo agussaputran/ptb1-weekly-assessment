d07df23591194adc59c258703f025d448ad34eda
rrb13 <ars.roybudiman@gmail.com>
2021-10-18 21:16:32 +0700

    revisi fungsi reversedString dan checkEvenOdd

--- week3/index.js
+++ week3/index.js
@@ -1,14 +1,25 @@
 const reversedString = (str) => {
     // process
+    //ini buat taruh string hasil reverse
+    var newString = ""
 
-    return // return a value
+    //for loop dari char paling belakang, 
+    for(var i = str.length - 1; i >= 0; i--) {
+        newString += str[i]
+    }
+    return newString// return a value
 }
 
 const checkEvenOdd = (number) => {
     // process
-
-    return // return a value
+    // even kalau angka habis dibagi 2 / num % 2 == 0, kalau tidak, jadi odd
+    if(number % 2 == 0) {
+        return (`${number} is even number`)
+    } else {
+        return (`${number} is odd number`)
+    }
+    
 }
 
-console.log(checkEvenOdd("pagi")); // expected output : igap
+console.log(reversedString("pagi")); // expected output : igap
 console.log(checkEvenOdd(7)); // expected output : 7 is Odd Number
