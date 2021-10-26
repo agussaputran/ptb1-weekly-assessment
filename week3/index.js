fe9689fb1deac5ba7e4817063391ae44ef8017d6
rrb13 <ars.roybudiman@gmail.com>
2021-10-26 20:12:04 +0800

    update index.js sumOfArray

--- week3/index.js
+++ week3/index.js
@@ -23,3 +23,19 @@ const checkEvenOdd = (number) => {
 
 console.log(reversedString("pagi")); // expected output : igap
 console.log(checkEvenOdd(7)); // expected output : 7 is Odd Number
+
+
+function sumOfArray(arr1, arr2) {
+    let result = 0
+    // the logic 
+    // type the code below
+    const reducer = (previousValue, currentValue) => previousValue + currentValue
+    const sumArray1 = arr1.reduce(reducer)
+    const sumArray2 = arr2.reduce(reducer)
+    result = sumArray1 + sumArray2
+    // type the code above
+    return result
+}
+
+console.log(sumOfArray([1,2,3,5], [1,2,3])) // expected output : 17
+console.log(sumOfArray([1,2,3], [1,2,3,2])) // expected output : 14
