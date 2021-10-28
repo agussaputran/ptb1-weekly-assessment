c9dbee08e2c6a390eaf9d726bd84ee8c8aa94248
donighazy <donighazy@gmail.com>
2021-10-28 14:08:08 +0800

    merapikan

--- tech-tryout1/index.js
+++ tech-tryout1/index.js
@@ -1,19 +1,21 @@
 function sumOfArray(arr1, arr2) {
-let a = arr1.length;
-let b = arr2.length;
+    let result = 0;
+    let a = arr1.length;
+    let b = arr2.length;
 
-for(let deadline=0; deadline<a; deadline++)
-{
- result = result + arr1[deadline];
-}
-
-for(let deadline=0; deadline<b; deadline++)
-{
- result = result + arr2[deadline];
-}
-
-return result
-}
+    
+    for(let deadline=0; deadline<a; deadline++)
+    {
+     result = result + arr1[deadline];
+    }
+    
+    for(let deadline=0; deadline<b; deadline++)
+    {
+     result = result + arr2[deadline];
+    }
+    
+    return result
+    }
 
 console.log(sumOfArray([1,2,3,5], [1,2,3])) // expected output : 17
 console.log(sumOfArray([1,2,3], [1,2,3,2])) // expected output : 14
